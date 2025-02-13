c201e0fb7377fde0c653a765b74b5ca6a695c27d
sharma.vishal009.11 <sharma.vishal009.11@gmail.com>
2025-02-13 19:44:30 +0530

    only storage code changed

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "rg-block" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "sto-block" {
+  name                     = "facebookstorage"
+  resource_group_name      = azurerm_resource_group.rg-block.name
+  location                 = azurerm_resource_group.rg-block.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 

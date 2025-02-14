13c614423a39c9f7ab113b7c8d9920b53fc29657
devops-2611 <emailtosfa@gmail.com>
2025-02-14 20:02:20 +0530

    ananya ka rg add hua hai abhi

--- cd.tf
+++ cd.tf
@@ -2,8 +2,7 @@ resource "azurerm_resource_group" "rg-block" {
     name = "welcome"
     location = "west us"
 }
-
 resource "azurerm_resource_group" "rg-block" {
-    name = "ananya"
+    name = "ananya-rg"
     location = "west us"
 }

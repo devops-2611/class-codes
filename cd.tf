7f1ac2ca9d230b816f5e25f6879397c2f73f951c
Sharma <emailtosfa@gmail.com>
2025-02-14 20:40:04 +0530

    Update cd.tf-faltu gaps removed

--- cd.tf
+++ cd.tf
@@ -2,16 +2,11 @@ resource "azurerm_resource_group" "rg-block" {
     name = "welcome"
     location = "west us"
 }
-
-
 resource "azurerm_resource_group" "rg-block" {
     name = "kriti-rg"
-
     location = "west us"
 }
-
 resource "azurerm_resource_group" "rg-block" {
     name = "ananya-rg"
-
     location = "west us"
 }

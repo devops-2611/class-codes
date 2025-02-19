ec7e836553bcb74ae9ee0be868704e0a01af8788
devops-2611 <emailtosfa@gmail.com>
2025-02-19 14:11:26 +0530

    all rg deleted except welcome

--- cd.tf
+++ cd.tf
@@ -2,11 +2,3 @@ resource "azurerm_resource_group" "rg-block" {
     name = "welcome"
     location = "west us"
 }
-resource "azurerm_resource_group" "rg-block" {
-    name = "kriti-rg"
-    location = "west us"
-}
-resource "azurerm_resource_group" "rg-block" {
-    name = "ananya-rg"
-    location = "west us"
-}

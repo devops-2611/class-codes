103b4667a8463cc2ec22ec6fb2cfabe9213790e1
devops-2611 <emailtosfa@gmail.com>
2025-02-19 14:23:09 +0530

    kriti

--- cd.tf
+++ cd.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg-block" {
     name = "welcome"
     location = "west us"
 }
+
+resource "azurerm_resource_group" "rg-block" {
+    name = "kriti"
+    location = "west us"
+}
